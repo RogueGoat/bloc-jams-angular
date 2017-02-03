f20b554b8978c5d1e4c9b7c61bfb656a6b8754ca
Florence <roguegoatstudios@outlook.com>
2017-02-02 20:54:39 -0600

    Checkpoint 10 progress complete

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -35,10 +35,22 @@
                   formats: ['mp3'],
                   preload: true
             });
+              
+          currentBuzzObject.bind('timeupdate', function() {
+              $rootScope.$apply(function() {
+              SongPlayer.currentTime = currentBuzzObject.getTime();
+              });
+     });
+
  
         SongPlayer.currentSong = song;
-              
+ /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+        SongPlayer.currentTime = null;
          };
+ 
 /**
  * @function playSong
  * @desc Private reusable function that plays a song
@@ -128,6 +140,16 @@
                         playSong(song);
               }
           };
+ /**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+        SongPlayer.setCurrentTime = function(time) {
+           if (currentBuzzObject) {
+           currentBuzzObject.setTime(time);
+           }
+       };
          
           return SongPlayer;
 }
